ea07cfaec7bff7116184275cd7d15d651803a370
Louis <huynguyennbk@gmail.com>
2022-05-15 01:19:59 +0700

    init

---
+++ lua/draft-buff/init.lua
@@ -0,0 +1,69 @@
+local Menu = require("nui.menu")
+local event = require("nui.utils.autocmd").event
+
+local function find_extension(name)
+  local mappings = { 
+    ["Ruby"] = ".rb",
+    ["Typescript"]  = ".ts",
+    ["Javascript"] = ".js",
+    ["Golang"] = ".go",
+    ["Java"] = ".java"
+  }
+
+  return mappings[name]
+end
+
+local menu = Menu({
+  position = "20%",
+  size = {
+    width = 30,
+    height = 5,
+  },
+  relative = "editor",
+  border = {
+    style = "single",
+    text = {
+      top = "Choose Something",
+      top_align = "center",
+    },
+  },
+  win_options = {
+    winblend = 10,
+    winhighlight = "Normal:Normal",
+  },
+}, {
+  lines = {
+    Menu.item("Ruby"),
+    Menu.item("Typescript"),
+    Menu.item("Javascript"),
+    Menu.item("Golang"),
+    Menu.item("Java"),
+  },
+  max_width = 30,
+  keymap = {
+    focus_next = { "j", "<Down>", "<Tab>" },
+    focus_prev = { "k", "<Up>", "<S-Tab>" },
+    close = { "<Esc>", "<C-c>" },
+    submit = { "<CR>", "<Space>" },
+  },
+  on_close = function()
+    print("CLOSED")
+  end,
+  on_submit = function(item)
+    local path = "/tmp/draft-buffer" .. find_extension(item["text"])
+
+    vim.cmd("e" .. path)
+  end,
+})
+
+local function select_lang()
+  -- mount the component
+  menu:mount()
+
+  -- close menu when cursor leaves buffer
+  menu:on(event.BufLeave, menu.menu_props.on_close, { once = true })
+end
+
+return {
+  select_lang = select_lang
+}
